2f38d39570af9f86e9d23b1138984a9614738917
voluzy <32136019+voluzy@users.noreply.github.com>
2017-12-18 12:45:51 +0000

    Update model.py

--- project/model.py
+++ project/model.py
@@ -11,6 +11,7 @@ import csv
 # create empty lists that are needed later     
 environment = []
 agents = []
+
 # set parameters of model
 num_of_agents = 50
 iterations = 100
@@ -19,18 +20,15 @@ neighbourhood = 20
 # open and read in environment file
 file = open('in.txt', newline='')
 reader = csv.reader(file, quoting=csv.QUOTE_NONNUMERIC)
-#append values to 2D list
-for row in reader:
+for row in reader:              #append values to 2D list
     rowlist = []
     for item in row:
         rowlist.append(item)
     environment.append(rowlist)
 file.close()
 
-# create figure with size
-fig = matplotlib.pyplot.figure(figsize = (7,7))
-# add fixed axes to figure
-ax = fig.add_axes([0,0,1,1])
+fig = matplotlib.pyplot.figure(figsize = (7,7))     # create figure with size
+ax = fig.add_axes([0,0,1,1])                        # add fixed axes to figure
 
 # create agents with attributes
 for i in range(num_of_agents):
@@ -38,14 +36,14 @@ for i in range(num_of_agents):
 
 # define an update function to create animation
 def update(frame_number):
-    fig.clear()                     # clear figure so next iteration can be displayed
-    random.shuffle(agents)          # randomise order in which agents are passed in
-    for i in range(num_of_agents):  # create for loop that calls the agent functions from agent framework
+    fig.clear()                             # clear figure so next iteration can be displayed
+    random.shuffle(agents)                  # randomise order in which agents are passed in
+    for i in range(num_of_agents):          # create for loop that calls the agent functions from agent framework
         agents[i].move()
         agents[i].eat()
         agents[i].vom()
         agents[i].share_with_neighbours(neighbourhood) 
-    matplotlib.pyplot.xlim(0, 300)  # set limits of plot
+    matplotlib.pyplot.xlim(0, 300)          # set limits of plot
     matplotlib.pyplot.ylim(0, 300)
     matplotlib.pyplot.imshow(environment)   # display the environment as background
     for i in range(num_of_agents):          # for loop to display each agents new position as scatterplot
